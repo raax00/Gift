2cb30ed46c367e963ccbeab74cd8e421aab5a93d
raax00 <raaxbhaii@gmail.com>
2026-04-02 08:22:42 +0530

    Create models/product.dart

---
+++ lib/models/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String id;
+  final String name;
+  final double price;
+  final String description;
+  final String imageUrl;
+  final String category;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.imageUrl,
+    required this.category,
+  });
+}
